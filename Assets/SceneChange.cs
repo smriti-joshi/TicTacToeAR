a9982b4755b88e5bca47919fb31bec813432e292
smriti-joshi <smriti161096@gmail.com>
2020-12-17 13:31:37 +0100

    Animation
    Clicking play button introduces cross fade animation for the first time.
    
    Doesn't do it later. Can't figure out why.

--- Assets/SceneChange.cs
+++ Assets/SceneChange.cs
@@ -7,12 +7,26 @@ using UnityEngine.UI;
 public class SceneChange : MonoBehaviour
 {
     // Start is called before the first frame update
-
+    public Animator transition;
     public SceneChange panel;
+    public float transitionTime = 5f;
 
-  //maybe make a function to disable button, argument in function
+    //maybe make a function to disable button, argument in function
     public void SampleScene()
     {
-        SceneManager.LoadScene("Assets/Scenes/SampleScene");
+        StartCoroutine(LoadSampleScene("Assets/Scenes/SampleScene"));
     }
+
+    IEnumerator LoadSampleScene(string scenePath)
+    {
+
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(scenePath);
+    }
+
+    /*public void DeleteSampleScene()
+    {
+        SceneManager.UnloadSceneAsync("Assets/Scenes/SampleScene");
+    }*/
 }
